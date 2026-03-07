2d24582ea5268fee253c9e2e08cac4c6286f2266
IT25100393 <it25100393@my.sliit.lk>
2026-03-07 15:04:45 +0530

    Initial base project setup

---
+++ MovieRentalSystem/src/test/java/com/movierentalsystem/MovieRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movierentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
